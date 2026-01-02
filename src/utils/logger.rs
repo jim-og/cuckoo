9b2998dcda035222778a93799f635376f68ae667
Jim Gartland <jim@danube.io>
2026-01-02 15:21:35 +0000

    Add optional receiver to StdoutLogger

--- src/utils/logger.rs
+++ src/utils/logger.rs
@@ -1,4 +1,9 @@
 use chrono::Utc;
+use tokio::sync::Mutex;
+use tokio::{
+    sync::mpsc::{self, Receiver, Sender},
+    time::{Duration, timeout},
+};
 
 #[derive(Debug, Clone, Copy)]
 pub enum LogLevel {
@@ -25,11 +30,68 @@ pub trait Logger: Send + Sync {
     }
 }
 
-pub struct StdoutLogger;
+pub struct StdoutLogger {
+    sender: Option<Sender<String>>,
+    receiver: Mutex<Option<Receiver<String>>>,
+}
+
+impl StdoutLogger {
+    pub fn new() -> Self {
+        Self {
+            sender: None,
+            receiver: Mutex::new(None),
+        }
+    }
+
+    pub fn with_receiver(mut self) -> Self {
+        let (sender, receiver) = mpsc::channel::<String>(1024);
+        self.sender = Some(sender);
+        self.receiver = Mutex::new(Some(receiver));
+        self
+    }
+
+    pub async fn contains(&self, needle: &str) -> bool {
+        let mut guard = self.receiver.lock().await;
+
+        let Some(receiver) = guard.as_mut() else {
+            return false;
+        };
+
+        loop {
+            match timeout(Duration::from_secs(2), receiver.recv()).await {
+                Ok(Some(msg)) => {
+                    if msg.contains(needle) {
+                        return true;
+                    }
+                }
+                Ok(None) => {
+                    // channel closed
+                    eprintln!("logger channel receiver closed");
+                    return false;
+                }
+                Err(_) => {
+                    // timeout elapsed
+                    eprintln!("logger channel timeout elapsed");
+                    return false;
+                }
+            }
+        }
+    }
+}
 
 impl Logger for StdoutLogger {
     fn log(&self, level: LogLevel, message: &str) {
         let timestamp = Utc::now().format("%Y-%m-%d %H:%M:%S%.3f");
-        println!("[{}] {:?}: {}", timestamp, level, message);
+        let msg = format!("[{}] {:?}: {}", timestamp, level, message);
+
+        println!("{}", msg);
+
+        if let Some(sender) = &self.sender {
+            let sender = sender.clone();
+            let msg = msg.clone();
+            tokio::spawn(async move {
+                let _ = sender.send(msg).await;
+            });
+        }
     }
 }
